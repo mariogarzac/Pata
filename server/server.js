73ba80e302c2b4886fbe40c82e196eca35f638ca
mariogarzac <mariogarzac99@gmail.com>
2023-04-06 19:18:16 -0600

    changes?

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
+// mongoose.connect("mongodb+srv://mariogarzac99:PEeV2mkaZxnjr0bb@pata-app-cluster.3croijz.mongodb.net/?retryWrites=true&w=majority");
+
 app.get("/api", (req, res) => {
-    res.json({"users" : ["userOne", "userTwo", "userThree"])
+    res.json({"users" : ["userOne", "userTwo", "userThree"]})
 })
 
 app.listen(5000, () => {console.log("Server running on port 5000")})
