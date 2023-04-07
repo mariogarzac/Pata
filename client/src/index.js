051e9ef871a5f8f12223114a94454fe2aa26a218
mariogarzac <mariogarzac99@gmail.com>
2023-04-06 21:59:13 -0600

    removed unused things

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import DynamicTextField from './DynamicTextField';
-import reportWebVitals from './reportWebVitals';
+import DynamicTextField from './components/DynamicTextField';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
     <DynamicTextField />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
